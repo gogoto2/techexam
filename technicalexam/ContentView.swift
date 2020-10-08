9e732b318d0955325519fde8f64be106d9b6723f
markcadag <markcadag@gmail.com>
2020-10-08 17:29:33 +0800

    Initial Commit

---
+++ technicalexam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  technicalexam
+//
+//  Created by iOS on 10/8/20.
+//  Copyright © 2020 iOS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
